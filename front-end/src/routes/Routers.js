7e23fb104ca8492c871649b4c5c2c82008262f52
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-06 13:54:26 -0300

    feat: create routes for pages

---
+++ front-end/src/routes/Routers.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Wallet from "../pages/Wallet";
+/* import Market from "../pages/Market";
+import Create from "../pages/Create";
+import Contact from "../pages/Contact";
+import NftDetails from "../pages/NftDetails"; */
+
+const Routers = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/wallet" element={<Wallet />} />
+{/*       <Route path="/market" element={<Market />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/market/:id" element={<NftDetails />} /> */}
+    </Routes>
+  );
+};
+
+export default Routers;
