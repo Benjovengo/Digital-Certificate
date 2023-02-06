b89667d94575eb7de034f79e1e3e157b458ca4dc
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-06 11:44:18 -0300

    tests: check preventing transfers

--- blockchain/test/IdentityToken.js
+++ blockchain/test/IdentityToken.js
@@ -42,9 +42,33 @@ describe('Identity ERC-721 Token', () => {
     expect(result).to.equal(identityManager.address);
   })
 
+  it('Prevent transfers', async () => {
+    // hard-coded setup for minting
+    const blockchainAddress = account01.address;
+    const identityURI = "path to the URI";
+    const hash = web3.utils.soliditySha3('Identity Hash');
+    const publicKey = "0xC74a9a98Af6108adD8EB17A4262d3dc9B924c429";
+
+    // minting transaction
+    await identityToken.mint(blockchainAddress, identityURI, hash, publicKey);
+
+    try {
+      await identityToken.transferFrom(account01.address, identityManager.address, 1)
+    } catch (error) {}
+
+    const result = await identityToken.ownerOf(1);
+    expect(result).to.equal(account01.address);
+  })
+
 })
 
+// TEMPLATES ==========================================================================================
+
 //console.log('\nOwner:    ', , '\n')
-//const mintTx = await identityToken.mint()
-//expect(result).to.equal("")
-//expect(result).to.not.equal("0x")
+
+/* it('', async () => {
+  await identityToken.
+  const result = await identityToken.owner();
+  console.log('\nOwner:    ', , '\n')
+  expect(result).to.equal(identityManager.address);
+}) */
