e8253234873d94ece63c27a1816ccf10bea72fae
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-06 17:34:34 -0300

    style: fix style of the Header

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -69,14 +69,12 @@ const Header = ({ account, setAccount }) => {
 
             <div className="nav__right d-flex align-items-center gap-3 ">
               <button className="btn d-flex gap-1 align-items-center"  onClick={connectHandler}>
-                <Link to="#">
                   {account? (
                     <div><i className="ri-wallet-line"></i> {account.slice(0,6) + '...' + account.slice(38,42)}</div>
                     //<p><b>Address:</b> {account}</p>
                   ) : (
                     <div><i className="ri-wallet-line"></i> Connect MetaMask</div>
                   )}
-                </Link>
               </button>
 
               <span className="mobile__menu">
