d47639a644c3c01d9a464e5496b3819bf92116ea
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-06 14:10:58 -0300

    feat: add components to the header

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import { Container } from "reactstrap";
+import { NavLink, Link } from "react-router-dom";
+
+import './header.css'
+
+
+const NAV__LINKS = [
+  {
+    display: "Home",
+    url: "/home",
+  },
+  {
+    display: "Wallet",
+    url: "/wallet",
+  },
+];
+
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <header className="header">
+        <Container>
+          <div className="navigation">
+            <div className="logo">
+              <h2 className=" d-flex gap-2 align-items-center ">
+                <span>
+                  <i className="ri-fire-fill"></i>
+                </span>
+                NFTs
+              </h2>
+            </div>
+
+            <div className="nav__menu">
+              <ul className="nav__list">
+                {NAV__LINKS.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <NavLink
+                      to={item.url}
+                      className={(navClass) =>
+                        navClass.isActive ? "active" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+          </div>
+        </Container>
+      </header>
+    </>
   )
 }
 
