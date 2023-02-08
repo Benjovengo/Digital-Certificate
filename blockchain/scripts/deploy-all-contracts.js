2626ad1b50aec077080c201cb24425741e401fc8
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-08 09:23:07 -0300

    fix: deploy-all-contracts working without logs

--- blockchain/scripts/deploy-all-contracts.js
+++ blockchain/scripts/deploy-all-contracts.js
@@ -1,21 +1,59 @@
 var exec = require('child_process').exec;
 
-exec('npx hardhat run deploy/01-IdentityToken.js --network localhost',
+
+const contractNames = [
+  'deploy/01-IdentityToken.js',
+  'deploy/02-IdentityManager.js'
+];
+
+const numberOfcontracts = contractNames.length;
+
+/* let command = '';
+
+for (var i = 0; i < numberOfcontracts; i++) {
+  command = command + `npx hardhat run ${contractNames[i]} --network localhost; `
+}
+
+console.log(command)
+
+exec(command,
   function (error, stdout, stderr) {
     console.log(stdout);
     if (error !== null) {
           console.log('exec error: ' + error);
     }
-});
+}); */
 
-exec('npx hardhat run deploy/02-IdentityManager.js --network localhost',
-  function (error, stdout, stderr) {
-    console.log(stdout);
-    if (error !== null) {
-          console.log('exec error: ' + error);
-    }
-});
 
+function execWrapper(command) {
+  return new Promise((resolve, reject) => {
+     exec(command, (error, out, err) => {
+       if (error) return reject(error);
+       resolve({out: out, err: err});
+     })
+  })
+}
+
+
+async function test() {
+  const contractNames = [
+    'deploy/01-IdentityToken.js',
+    'deploy/02-IdentityManager.js'
+  ];
+  for (var i in contractNames) {
+          try{
+              console.log(contractNames[i])
+              command = `npx hardhat run ${contractNames[i]} --network localhost`
+              var promise = execWrapper(command)//any promise, doesn't matter now 
+              await promise;
+           }catch(err) {
+            console.log("Promise Rejected");
+            console.log(err)
+           }
+   }
+}
+
+test()
 
     /* await runScript("../deploy/01-IdentityToken.js");
     await runScript("../deploy/02-IdentityManager.js"); */
