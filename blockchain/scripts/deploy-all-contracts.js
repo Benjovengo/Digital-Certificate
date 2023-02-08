6c9900428120b7c72c43a031b6fce679365fae24
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-08 08:32:57 -0300

    refactor code for deployment of all contracts

--- blockchain/scripts/deploy-all-contracts.js
+++ blockchain/scripts/deploy-all-contracts.js
@@ -1,22 +1,17 @@
-var exec = require('child_process').exec;
+const exec = require('child_process').exec;
 
-exec('npx hardhat run deploy/01-IdentityToken.js --network localhost',
-  function (error, stdout, stderr) {
-    console.log(stdout);
-    if (error !== null) {
-          console.log('exec error: ' + error);
-    }
-});
+const contracts = [
+  'deploy/01-IdentityToken.js',
+  'deploy/02-IdentityManager.js'
+]
 
-exec('npx hardhat run deploy/02-IdentityManager.js --network localhost',
-  function (error, stdout, stderr) {
-    console.log(stdout);
-    if (error !== null) {
-          console.log('exec error: ' + error);
-    }
-});
-
-
-    /* await runScript("../deploy/01-IdentityToken.js");
-    await runScript("../deploy/02-IdentityManager.js"); */
+for (const contract of contracts) {
+  exec(`npx hardhat run ${contract} --network localhost`,
+    function (error, stdout, stderr) {
+      console.log(stdout);
+      if (error !== null) {
+            console.log('exec error: ' + error);
+      }
+  });
+}
 
