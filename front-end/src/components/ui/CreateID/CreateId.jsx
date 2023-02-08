6b474399e0ff0a15b73bb9070535a78d2bb99493
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-08 12:13:34 -0300

    add CreateId boilerplate

---
+++ front-end/src/components/ui/CreateID/CreateId.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+/// Style
+import "./create-id.css"
+
+const CreateId = () => {
+  return (
+    <>
+      <div>CreateId</div>
+    </>
+  )
+}
+
+export default CreateId
--- front-end/src/pages/Identity.jsx
+++ front-end/src/pages/Identity.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 // TEMP
 import Cryptography from '../components/ui/Cryptography/Cryptography'
+import CreateId from '../components/ui/CreateID/CreateId'
 
 const Identity = () => {
   return (
     <>
       <div>Identity</div>
       <Cryptography />
+      <CreateId />
     </>
   )
 }
