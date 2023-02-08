b2117ff7246a6b9945e1215c8bbaab4be7d2bc76
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-08 08:50:25 -0300

    bug: async problem

--- blockchain/scripts/deploy-all-contracts.js
+++ blockchain/scripts/deploy-all-contracts.js
@@ -1,15 +1,28 @@
 const exec = require('child_process').exec;
 
-const contracts = [
-  'deploy/01-IdentityToken.js'
-]
 
-contracts.forEach( function(contract, index) {
-  exec(`npx hardhat run ${contract} --network localhost`,
+const deploy = async (_file) => {
+  console.log(`npx hardhat run ${_file} --network localhost`)
+  exec(`npx hardhat run ${_file} --network localhost`,
     function (error, stdout, stderr) {
       console.log(stdout);
       if (error !== null) {
             console.log('exec error: ' + error);
       }
   });
-});
+}
+
+const deployAll = async () => {
+  const contracts = [
+    'deploy/01-IdentityToken.js',
+    'deploy/02-IdentityManager.js'
+  ];
+  const numberOfContracts = contracts.length;
+
+  for (var i = 0; i < numberOfContracts; i++) {
+    await deploy(contracts[i]);
+  }
+
+}
+
+deployAll()
