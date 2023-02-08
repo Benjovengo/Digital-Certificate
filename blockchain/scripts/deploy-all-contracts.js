5f4fbb89da6cffa5753dcbd586e327c25eef8a31
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-08 09:09:31 -0300

    bug: can't deploy all

--- blockchain/scripts/deploy-all-contracts.js
+++ blockchain/scripts/deploy-all-contracts.js
@@ -1,28 +1,21 @@
-const exec = require('child_process').exec;
+var exec = require('child_process').exec;
+
+exec('npx hardhat run deploy/01-IdentityToken.js --network localhost',
+  function (error, stdout, stderr) {
+    console.log(stdout);
+    if (error !== null) {
+          console.log('exec error: ' + error);
+    }
+});
+
+exec('npx hardhat run deploy/02-IdentityManager.js --network localhost',
+  function (error, stdout, stderr) {
+    console.log(stdout);
+    if (error !== null) {
+          console.log('exec error: ' + error);
+    }
+});
 
 
-const deploy = async (_file) => {
-  console.log(`npx hardhat run ${_file} --network localhost`)
-  exec(`npx hardhat run ${_file} --network localhost`,
-    function (error, stdout, stderr) {
-      console.log(stdout);
-      if (error !== null) {
-            console.log('exec error: ' + error);
-      }
-  });
-}
-
-const deployAll = async () => {
-  const contracts = [
-    'deploy/01-IdentityToken.js',
-    'deploy/02-IdentityManager.js'
-  ];
-  const numberOfContracts = contracts.length;
-
-  for (var i = 0; i < numberOfContracts; i++) {
-    await deploy(contracts[i]);
-  }
-
-}
-
-deployAll()
+    /* await runScript("../deploy/01-IdentityToken.js");
+    await runScript("../deploy/02-IdentityManager.js"); */
