2493392c9554213814f702bf2d9c3e5ad102c2de
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-10 16:44:54 -0300

    add style to form

--- front-end/src/components/ui/CreateCertification/CreateCertification.jsx
+++ front-end/src/components/ui/CreateCertification/CreateCertification.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Container, Row, Col } from "reactstrap";
 
 /// Style
@@ -8,55 +8,60 @@ import "./create-certification.css"
 
 const CreateCertification = () => {
   
-  
 
   return (
     <>
-    <section className='create__id__wrapper'>
-        <Container fluid>
+    <section className='create__certification__wrapper'>
+        <Container>
           <Row>
-            <h1>CreateCertification</h1>
+            <h1>Create Certification</h1>
               <Col>
                 <div className="form__wrapper">
                   <Row>
                     <Col>
                       <label htmlFor="institution">Institution</label><br/>
-                      <input type="text" id="institution" name="institution" maxLength="100" required></input>
+                      <input type="text" id="institution" name="institution" maxLength="100" required/>
                     </Col>
                   </Row>
                   <Row>
                     <Col>
-                      <label htmlFor="title">Title</label><br/>
-                      <input type="text" id="title" name="title" maxLength="200" defaultValue={'Sliding mode approaches for the longitudinal control of an autonomous robotic blimp'}></input>
+                      <label htmlFor="blockchainAddress">Address</label><br/>
+                      <input type="text" id="blockchainAddress" name="blockchainAddress" maxLength="42" placeholder='0x000000000000000000000000000000000000000000000000' required />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col>
+                      <label htmlFor="workTitle">Title</label><br/>
+                      <input type="text" id="workTitle" name="workTitle" maxLength="200" placeholder='Sliding mode approaches for the longitudinal control of an autonomous robotic blimp'/>
                     </Col>
                   </Row>
                   <Row>
                     <Col>
                       <label htmlFor="advisor">Advisor</label><br/>
-                      <input type="text" id="advisor" name="advisor" maxLength="200"></input>
+                      <input type="text" id="advisor" name="advisor" maxLength="200"/>
                     </Col>
                     <Col>
                       <label htmlFor="coAdvisor">Co-advisor</label><br/>
-                      <input type="text" id="coAdvisor" name="coAdvisor" maxLength="200"></input>
+                      <input type="text" id="coAdvisor" name="coAdvisor" maxLength="200"/>
                     </Col>
                   </Row>
                   <Row>
                     <Col>
                       <label htmlFor="degree">Degree</label><br/>
-                      <input type="text" id="degree" name="degree" maxLength="200"></input>
+                      <input type="text" id="degree" name="degree" maxLength="200"/>
                     </Col>
                     <Col>
                       <label htmlFor="gpa">GPA</label><br/>
-                      <input type="number" id="gpa" name="gpa"></input>
+                      <input type="number" id="gpa" name="gpa" placeholder='4.0'/>
                     </Col>
                     <Col>
                       <label htmlFor="date">Date</label><br/>
-                      <input type="date" id="date" name="date" disabled></input>
+                      <input type="date" id="date" name="date" disabled/>
                     </Col>
                   </Row>
                   <Row>
-                    <Col>
-                      <button className='submit'>Add certificate</button>
+                    <Col className='mt-5 d-flex justify-content-end'>
+                      <button className='submit__btn submit'>Add certificate</button>
                     </Col>
                   </Row>
                 </div>
