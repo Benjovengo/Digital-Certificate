0cd1704c52ea4306ea4062ca5b0cebefe4e08542
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-10 08:13:45 -0300

    back to older version

--- front-end/src/scripts/identity/uploadJsonIPFS.jsx
+++ front-end/src/scripts/identity/uploadJsonIPFS.jsx
@@ -1,69 +1,36 @@
-import { ethers } from 'ethers';
-import { Buffer } from 'buffer';
-
-
-// Encryption
-import { encryptData } from "./cryptography";
-
 // Pinata
 import axios from "axios";
-const FormData = require('form-data')
+//const FormData = require('form-data')
 
+const uploadJSONtoIPFS = async (_firstName, _lastName, _imgURL, _issuedBy, _dateIssued) => {
+  let tokenURI
+  const plainData = {
+    "firstName": _firstName,
+    "lastName": _lastName,
+    "image": _imgURL,
+    "issuedBy": _issuedBy,
+    "dateIssued": _dateIssued
+  }
+  try {
+    const resJSON = await axios({
+      method: "post",
+      url: "https://api.pinata.cloud/pinning/pinJsonToIPFS",
+      data: plainData,
+      headers: {
+        'pinata_api_key': `${process.env.REACT_APP_PINATA_API_KEY}`,
+        'pinata_secret_api_key': `${process.env.REACT_APP_PINATA_API_SECRET}`,
+      },
+    });
 
-  const uploadJSONtoIPFS = async (_firstName, _lastName, _imgURL, _issuedBy, _dateIssued) => {
+    tokenURI = `https://gateway.pinata.cloud/ipfs/${resJSON.data.IpfsHash}`;
+    //console.log("Token URI", tokenURI);
+    //mintNFT(tokenURI, currentAccount)   // pass the winner
 
-    let ImgHash
-
-    try {
-
-      const plainData = 'Fabio'
-
-      const formData = new FormData();
-      const base64Data = 'aGVsbG8gd29ybGQ='; // "hello world" encoded in base64
-      formData.append('file', new Blob([base64Data], { type: 'application/octet-stream' }), 'hello.dat');
-
-      console.log(formData)
-
-
-
-      const resFile = await axios({
-        method: "post",
-        url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
-        data: formData,
-        headers: {
-          'pinata_api_key': `${process.env.REACT_APP_PINATA_API_KEY}`,
-          'pinata_secret_api_key': `${process.env.REACT_APP_PINATA_API_SECRET}`,
-          "Content-Type": "multipart/form-data"
-        },
-      });
-
-      ImgHash = `https://gateway.pinata.cloud/ipfs/${resFile.data.IpfsHash}`;
-      //console.log(response.data.IpfsHash);
-      //console.log(ImgHash)
-
-
-    } catch (error) {
-        console.log("File to IPFS: ")
-        console.log(error)
-    }
-  return ImgHash
+  } catch (error) {
+      console.log("JSON to IPFS: ")
+      console.log(error);
+  }
+  return tokenURI
 }
 
 export default uploadJSONtoIPFS
-
-
-
-
-const getPublicKey = async () => {
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-  const account = ethers.utils.getAddress(accounts[0])
-
-  // Key is returned as base64
-  const keyB64 = await window.ethereum.request({
-    method: 'eth_getEncryptionPublicKey',
-    params: [account],
-  });
-  const publicKey = Buffer.from(keyB64, 'base64');
-
- return publicKey
-}
