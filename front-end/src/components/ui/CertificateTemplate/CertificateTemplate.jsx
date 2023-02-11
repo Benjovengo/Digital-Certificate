4fc63db4afb0e2b4290d5d028e61b6eb82ce89f9
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-11 15:28:43 -0300

    add code to export diploma to pdf

--- front-end/src/components/ui/CertificateTemplate/CertificateTemplate.jsx
+++ front-end/src/components/ui/CertificateTemplate/CertificateTemplate.jsx
@@ -1,13 +1,40 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Container, Row, Col } from "reactstrap";
+import html2pdf from 'html2pdf.js'
 
 import './certificate-template.css'
+  
+
 
 const CertificateTemplate = ({ institution, fullName, blockchainAddress, degree, area, advisor }) => {
+
+
+
+  /**
+   * Export certificate to pdf
+   */
+  function handleExport() {
+    // Choose the element id which you want to export.
+    var element = document.getElementById('divToExport');
+    /* element.style.width = '700px';
+    element.style.height = '900px'; */
+    var opt = {
+        margin:       0.5,
+        filename:     `Certificate_${fullName.replace(/\s+/g, '')}_from.pdf`,
+        image:        { type: 'jpeg', quality: 1 },
+        html2canvas:  { scale: 1 },
+        jsPDF:        { unit: 'in', format: 'letter', orientation: 'landscape',precision: '12' }
+      };
+    
+    // choose the element and pass it to html2pdf() function and call the save() on it to save as pdf.
+    html2pdf().set(opt).from(element).save();
+  }
+
+
   return (
     <>
     <Container>
-      <div className='template__wrapper'>
+      <div className='template__wrapper' id="divToExport">
         <Row>
           <Col>
             <h1>{institution}</h1>
@@ -19,8 +46,8 @@ const CertificateTemplate = ({ institution, fullName, blockchainAddress, degree,
           </Col>
         </Row>
       </div>
-      
     </Container>
+    <button onClick={handleExport}>Export to PDF</button>
 
     </>
   )
