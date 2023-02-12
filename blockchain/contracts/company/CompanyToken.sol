fad5c6eb0a92606581b43fa9dce69e03e9996567
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-12 19:53:37 -0300

    update compiler version

--- blockchain/contracts/company/CompanyToken.sol
+++ blockchain/contracts/company/CompanyToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17;
 
 import "../../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "../../node_modules/@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
