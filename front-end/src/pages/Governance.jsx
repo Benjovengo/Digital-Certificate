2dcb3eef6dd635e4411f74351afb8e7683a6cd6f
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-13 07:48:46 -0300

    debug: add cryptography react component

--- front-end/src/pages/Governance.jsx
+++ front-end/src/pages/Governance.jsx
@@ -1,5 +1,11 @@
 import React, { useLayoutEffect } from 'react'
 
+
+// DEBUG - cryptography
+import Cryptography from '../components/ui/Cryptography/Cryptography';
+
+
+
 const Governance = () => {
 
   /** Start the page at the top */
@@ -9,7 +15,10 @@ const Governance = () => {
 
   
   return (
-    <div>Governance</div>
+    <>
+      <h1>Governance</h1>
+      <Cryptography />
+    </>
   )
 }
 
