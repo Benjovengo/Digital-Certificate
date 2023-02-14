64609d4feaeea840551b7979df7507d519c4389e
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-14 11:09:41 -0300

    add placeholder to return the score for an account

--- blockchain/contracts/governance/score/ScoreInfo.sol
+++ blockchain/contracts/governance/score/ScoreInfo.sol
@@ -19,17 +19,6 @@ contract ScoreInfo {
     /**
      * State variables
      */
-    /// @notice Each certificate is assigned to a blockchain address and
-    ///         each blockchain address may have several certificates.
-    /// The relevant information of the certificate for calculating the
-    /// score are the level of the certificate and the corresponding GPA.
-    /// @dev mapping from address to a mapping from certificate_index to
-    ///      its [level, gpa] (array)
-    mapping(address => mapping(uint256 => uint16)) private certificateDetails;
-    /// Stores the total number of certificates an address has
-    /// @dev Used to track the certificateDetails for all certificates
-    ///      issued to a particular blockchain address
-    mapping(address => uint256) private numberOfCertificates;
 
     /// Contracts
     ExpertiseClusters private expertiseClusters;
@@ -52,12 +41,9 @@ contract ScoreInfo {
      * Functions and Methods
      */
 
-    /// Read Score
-    function readScoreElement(uint256 _certificate, uint256 _gpa)
-        public
-        pure
-        returns (uint256)
-    {
-        return _certificate + _gpa;
+    /// Return the score of an account
+    function accountScore(address _account) public view returns (uint256) {
+        uint256 total = certificateManager.getNumberOfCertificates(_account);
+        return total;
     }
 }
