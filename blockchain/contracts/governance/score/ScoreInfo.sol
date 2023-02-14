3502fdde399e8a4b8ba10e0b7552362afb23352c
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-14 08:34:55 -0300

    struct: add score contract boilerplate

--- blockchain/contracts/governance/score/ScoreInfo.sol
+++ blockchain/contracts/governance/score/ScoreInfo.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+/// @dev Import the clusters contract to get the weights and the clusters' limits
+import "../ExpertiseClusters.sol";
+
 /**
  * @title Score Information - Governance (Digital Certificate)
  * @author Fábio Benjovengo
@@ -11,5 +14,21 @@ pragma solidity ^0.8.17;
  * @custom:security-contact fabio.benjovengo@gmail.com
  */
 contract ScoreInfo {
+    /**
+     * State variables
+     */
+    /// Contracts
+    ExpertiseClusters public expertiseClusters;
 
+    /**
+     * Functions and Methods
+     */
+
+    /// Read Score
+    function readScoreElement(uint256 _certificate, uint256 _gpa)
+        public
+        returns (uint256)
+    {
+        return _certificate + _gpa;
+    }
 }
