1da107ad3f8c0061e4539f087abb15282a7c4337
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-14 19:36:13 -0300

    struct add deployment file for the governance contracts

---
+++ blockchain/deploy/03-governance.js
@@ -0,0 +1,7 @@
+/**
+ * @title Deployment script for the Governance Contracts
+ * @author Fábio Benjovengo
+ * 
+ * @dev Deployment scripts for the contracts responsible
+ *      for the governance of the system.
+ */
