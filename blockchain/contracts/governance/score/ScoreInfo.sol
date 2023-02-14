ccc65e0c770241fc2984ae7ca6c2ef60bb0f3ff7
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-14 09:42:08 -0300

    import Certificate Manager

--- blockchain/contracts/governance/score/ScoreInfo.sol
+++ blockchain/contracts/governance/score/ScoreInfo.sol
@@ -3,6 +3,8 @@ pragma solidity ^0.8.17;
 
 /// @dev Import the clusters contract to get the weights and the clusters' limits
 import "../ExpertiseClusters.sol";
+/// @dev Import the certificate manager to have access to the number of certificates owned by an account
+import "../../certificate/CertificateManager.sol";
 
 /**
  * @title Score Information - Governance (Digital Certificate)
@@ -30,15 +32,20 @@ contract ScoreInfo {
     mapping(address => uint256) private numberOfCertificates;
 
     /// Contracts
-    ExpertiseClusters public expertiseClusters;
+    ExpertiseClusters private expertiseClusters;
+    CertificateManager private certificateManager;
 
     /**
      * Constructor Method
      *
      * @param _expertiseClusterAddress the address of the ExpertiseClusters contract on the blockchain (after deployment)
      */
-    constructor(address _expertiseClusterAddress) {
+    constructor(
+        address _expertiseClusterAddress,
+        address _certificateManagerAddress
+    ) {
         expertiseClusters = ExpertiseClusters(_expertiseClusterAddress);
+        certificateManager = CertificateManager(_certificateManagerAddress);
     }
 
     /**
