098534f416d5f986e32b77989ffd3f33410c1bd6
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-14 08:14:11 -0300

    struct: add boilerplate to the score contract

---
+++ blockchain/contracts/governance/score/ScoreInfo.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+/**
+ * @title Score Information - Governance (Digital Certificate)
+ * @author Fábio Benjovengo
+ *
+ * @notice Implementation of the score contract to retrieve data
+ *         about the score and cluster for a particular case.
+ *
+ * @custom:security-contact fabio.benjovengo@gmail.com
+ */
+contract ScoreInfo {
+
+}
