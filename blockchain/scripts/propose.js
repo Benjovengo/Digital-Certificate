d7da1607161ecc2ad5027d8d7756af9126e6b7c9
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-14 18:26:38 -0300

    struct: add placeholders for governance actions

---
+++ blockchain/scripts/propose.js
@@ -0,0 +1,13 @@
+/**
+ * @title Proposal
+ * @author Fábio Benjovengo
+ * 
+ * @notice Propose an action to be voted
+ * 
+ * @dev Usage: hardhat run scripts/propose.js --network localhost
+ * @dev It is important not to forget the --network localhost to run this script
+ * 
+ * @dev The OpenZeppelin Governor contract prevents the same proposal to be submitted more than once
+ * 
+ */
+
---
+++ blockchain/scripts/vote.js
@@ -0,0 +1,10 @@
+/**
+ * @title Proposal
+ * @author Fábio Benjovengo
+ * 
+ * @notice Cast a vote for a propose
+ * 
+ * @dev Usage: hardhat run scripts/vote.js --network localhost
+ * @dev It is important not to forget the --network localhost to run this script
+ * 
+ */
