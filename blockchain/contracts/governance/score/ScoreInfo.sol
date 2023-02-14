6483dddcafafa50a4526857d081d010f1a116bb7
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-14 08:15:02 -0300

    chore: improve comment formatting

--- blockchain/contracts/governance/score/ScoreInfo.sol
+++ blockchain/contracts/governance/score/ScoreInfo.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.17;
  * @title Score Information - Governance (Digital Certificate)
  * @author Fábio Benjovengo
  *
- * @notice Implementation of the score contract to retrieve data
- *         about the score and cluster for a particular case.
+ * @notice Implementation of the contract to retrieve data about
+ *         the score and cluster for a particular case.
  *
  * @custom:security-contact fabio.benjovengo@gmail.com
  */
