84b259c8d8bbea1788dc6d381ac3ab1161fc80a8
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-14 07:00:55 -0300

    docs: improve comment

--- blockchain/contracts/governance/ExpertiseClusters.sol
+++ blockchain/contracts/governance/ExpertiseClusters.sol
@@ -5,10 +5,17 @@ pragma solidity ^0.8.17;
 import "../../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 /**
- * @title GPA Clusters - Governance (Digital Certificate)
+ * @title Expertise Clusters - Governance (Digital Certificate)
  * @author Fábio Benjovengo
  *
- * @notice Define the GPA grades to be in one of three levels: novice, intermediate, or expert.
+ * Define the expertise level based on the certificates and the corresponding GPA
+ * grades to be in one of three levels: novice, intermediate, or expert.
+ *
+ * @notice Total score: the weighted sum of the certificates and its corresponding GPAs
+ * The weights of the certificates are based on the level of the certificate
+ * The GPAs are also weighted
+ * The total score entry is: <weight><certificate_level>*<GPA>,
+ * where the weight is controlled by the governance
  *
  * @dev Use this contract only for tests! Do NOT send any real ether to this project!
  * @custom:experimental This is an experimental contract.
