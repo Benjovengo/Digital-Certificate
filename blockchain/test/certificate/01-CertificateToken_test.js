43264eb6c1cdd718c8cd6ecff56b3807cee81844
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-14 13:00:01 -0300

    update testing of the certificate token

--- blockchain/test/certificate/01-CertificateToken_test.js
+++ blockchain/test/certificate/01-CertificateToken_test.js
@@ -3,12 +3,12 @@ const { ethers } = require('hardhat');
 const web3 = require('web3');
 
 describe('Certificate ERC-721 Token', () => {
-  let deployer, account01, certificateManager;
+  let deployer, account01, account02, certificateManager;
   let certificateToken;
 
   beforeEach(async () => {
     // Setup accounts - to get signers use `const signers = await ethers.getSigners()`
-    [deployer, account01, certificateManager] = await ethers.getSigners();
+    [deployer, account01, account02, certificateManager] = await ethers.getSigners();
 
     // Deploy CertificateToken
     const CertificateToken = await ethers.getContractFactory('CertificateToken');
@@ -58,6 +58,21 @@ describe('Certificate ERC-721 Token', () => {
 
     const result = await certificateToken.ownerOf(1);
     expect(result).to.equal(account01.address);
+  })
+
+  it('Get serial number of a certificate', async () => {
+    // hard-coded setup for minting
+    const blockchainAddress = [account01.address, account01.address, account02.address];
+    const certificateURI = "path to the URI";
+    const hash = '0x9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08';
+    const publicKey = "0xC74a9a98Af6108adD8EB17A4262d3dc9B924c429";
+
+    for (let i = 0; i < blockchainAddress.length; i++) {
+      // minting transaction
+      await certificateToken.mint(blockchainAddress[i], certificateURI, hash, publicKey);
+    }
+    const result = await certificateToken.getSerialNumber(account02.address, 0); // start at index ZERO
+    expect(result).to.equal(3);
   })
 
 })
