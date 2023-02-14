851fe4f2bba90475a165f13aedf25677deb548b4
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-14 19:07:18 -0300

    add scripts to 'accelerate' the blocks in hre

---
+++ blockchain/scripts/utils/speedUpTime.js
@@ -0,0 +1,11 @@
+/// @notice Move blocks forward in the localhost
+const fastForwardBlocks = async (_amount) => {
+  const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545');
+  for (let i = 0; i < _amount; i++) {
+    await provider.send("evm_mine", []);
+  }
+
+  console.log(`Moved forward ${_amount} blocks.`);
+};
+
+module.exports = fastForwardBlocks;
---
+++ blockchain/scripts/utils/speedUpTimeSeconds.js
@@ -0,0 +1,8 @@
+/// @notice Move time forward in the localhost
+const speedUpSeconds = async (_seconds) => {
+  const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545');
+  await provider.send("evm_increaseTime", [_seconds]);
+  console.log(`Moved forward ${_seconds} seconds.`);
+};
+
+module.exports = speedUpSeconds;
