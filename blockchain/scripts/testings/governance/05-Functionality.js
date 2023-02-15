6e23832f764eb7f1e90c1d47ddbcaaa252ea5b2c
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-15 15:01:06 -0300

    add call to propose script

--- blockchain/scripts/testings/governance/05-Functionality.js
+++ blockchain/scripts/testings/governance/05-Functionality.js
@@ -1,12 +1,15 @@
 const { expect } = require('chai')
 const { ethers } = require('hardhat')
 
+const proposeAction = require( '../../propose')
+
 const governanceFunctionality = async () => {
   describe('Functionality Test', () => {
     // Run before each test
     beforeEach(async () => {
       // global.expertiseClusters
-      
+      const proposalId = await proposeAction()
+      console.log(proposalId)
     })
 
 
