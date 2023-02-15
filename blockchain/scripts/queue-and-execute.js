e111a54c85dc764eebf224e83dfa0350d980257f
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-15 18:55:47 -0300

    add queue and execute boilerplate

--- blockchain/scripts/queue-and-execute.js
+++ blockchain/scripts/queue-and-execute.js
@@ -7,3 +7,15 @@
  * @dev Usage: hardhat run scripts/queue-and-execute.js --network localhost
  * @dev It is important not to forget the --network localhost to run this script
  */
+const hre = require("hardhat");
+const fs = require("fs"); // to copy the files to be used by the web interface
+
+const fastForwardBlocks = require("./utils/speedUpTime.js")
+const speedUpSeconds = require("./utils/speedUpTimeSeconds.js")
+
+
+const queueAndExecute = async () => {
+
+}
+
+module.exports = queueAndExecute
