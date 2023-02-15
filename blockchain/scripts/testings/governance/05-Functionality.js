a7199a0269c7faf7c8f4a68964ae62b998a76923
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-15 19:15:50 -0300

    BUG: test if proposal status is 1:active

--- blockchain/scripts/testings/governance/05-Functionality.js
+++ blockchain/scripts/testings/governance/05-Functionality.js
@@ -13,13 +13,12 @@ const governanceFunctionality = async () => {
     it('Add a proposal (proposal state = 0: Pending).', async () => {
       proposal = await proposeAction()
       const proposalState = proposal[1]
-      expect(proposalState).to.equal(0)
-      /* votingResult = await voteAction()
-      console.log('Proposal state: ', votingResult) */
+      expect(proposalState).to.equal(1)
     })
 
     it('Vote (proposal state = : ).', async () => {
-      
+      /* votingResult = await voteAction()
+      console.log('Proposal state: ', votingResult) */
     })
 
     it('Queue and Execute.', async () => {
