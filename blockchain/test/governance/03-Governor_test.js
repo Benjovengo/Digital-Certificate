c30e7c2f9ff5b1215ed7d5cca10a7a20a2210a5e
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-15 08:55:35 -0300

    add setup

--- blockchain/test/governance/03-Governor_test.js
+++ blockchain/test/governance/03-Governor_test.js
@@ -24,6 +24,10 @@ describe('Governor Contract', () => {
     const TimeLock = await ethers.getContractFactory('TimeLock')
     timeLock = await TimeLock.deploy(MIN_DELAY, PROPOSERS, EXECUTORS)
 
+
+    /**
+     * Governor Contract Deployment
+     */
     /// Definitions for the Governor Contract
     const VOTING_DELAY = 1; // blocks
     const VOTING_PERIOD = 5; // blocks
@@ -32,6 +36,24 @@ describe('Governor Contract', () => {
     /// Deploy Governor contract
     const GovernorContract = await ethers.getContractFactory('GovernorContract')
     governorContract = await GovernorContract.deploy(votingToken.address, timeLock.address, VOTING_DELAY, VOTING_PERIOD, QUORUM_PERCENTAGE)
+
+    /**
+     * Governor Setup
+     */
+    /// Roles setup
+    const proposerRole = await timeLock.PROPOSER_ROLE();
+    const executorRole = await timeLock.EXECUTOR_ROLE();
+    const adminRole = await timeLock.TIMELOCK_ADMIN_ROLE();
+
+    const proposerTx = await timeLock.grantRole(proposerRole, governorContract.address);
+    await proposerTx.wait(1);
+
+    const executorTx = await timeLock.grantRole(executorRole, "0x0000000000000000000000000000000000000000");
+    await executorTx.wait(1);
+
+    /// make nobody the admin
+    const revokeTx = await timeLock.revokeRole(adminRole, deployer.address);
+    await revokeTx.wait(1);
   })
 
   it('Deployment address.', async () => {
