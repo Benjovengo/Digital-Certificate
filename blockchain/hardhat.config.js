325b2294db140e4e3951dc985948dc38a7b9544c
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-15 15:00:46 -0300

    add network config

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -2,6 +2,16 @@ require('@nomicfoundation/hardhat-toolbox')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    localhost: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    }
+  },
   solidity: {
     version: '0.8.17',
     settings: {
