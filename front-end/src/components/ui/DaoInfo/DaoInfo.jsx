75bf32ffc2899e416bf63ddc422c9a39b4c822b6
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-16 19:58:50 -0300

    improve boilerplate

--- front-end/src/components/ui/DaoInfo/DaoInfo.jsx
+++ front-end/src/components/ui/DaoInfo/DaoInfo.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Container, Row, Col } from "reactstrap";
 
+// CSS Style
+import './DaoInfo.css'
+
+/**
+ * DAO Information/Parameters
+ * 
+ * @returns Display the parameters of the DAO
+ */
 const DaoInfo = () => {
+
+  /**
+   * Return the page elements based on the ExpertiseClusters smart contract
+   */
   return (
     <>
-      <h1>Dao Information</h1>
+      <section className='dao__wrapper'>
+        <Container fluid>
+          <Row>
+            <Col>
+              <h1>Dao Information</h1>
+              <h2>Expertise Levels</h2>
+              <h2>Your Expertise</h2>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </>
   )
 }
