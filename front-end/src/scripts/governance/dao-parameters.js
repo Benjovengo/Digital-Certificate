aecc574cf340cbedbd6245235e73fb8812da3a1c
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-17 07:17:50 -0300

    add function to retrieve the weights from the DAO

---
+++ front-end/src/scripts/governance/dao-parameters.js
@@ -0,0 +1,29 @@
+import { ethers } from 'ethers'
+
+/** Contract(s) and Address(es) */
+import ExpertiseClusters from '../../abis/ExpertiseClusters.json' // contract ABI
+import config from '../../config.json' // contract addresses
+
+/**
+ * Fetch the parameters defined by the governance
+ * 
+ * @return The weights for each of the different certification levels
+ */
+const fetchDaoParams = async () => {
+  // Setup provider and network
+  const provider = new ethers.providers.Web3Provider(window.ethereum)
+  const network = await provider.getNetwork()
+  const signer = provider.getSigner() // get the signer
+
+  /// Javascript "version" of the smart contract
+  const expertiseClusters = new ethers.Contract(config[network.chainId].expertiseClusters.address, ExpertiseClusters, signer)
+
+  let weight = []
+  for (let i = 0; i < 3; i++) {
+    weight.push(await expertiseClusters.retrieveWeight(i))    
+  }
+
+  return weight
+}
+
+module.exports = fetchDaoParams
