4bad1a9e6c2a4220d2864bb068f85a1238dfffd6
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-17 11:02:53 -0300

    increase number of blocks and time

--- front-end/src/scripts/utils/speedUpTime.js
+++ front-end/src/scripts/utils/speedUpTime.js
@@ -1,13 +1,29 @@
-const { ethers } = require('hardhat')
+import { ethers } from 'ethers'
 
-/// @notice Move blocks forward in the localhost
-const fastForwardBlocks = async (_amount) => {
+
+/**
+ * Move blocks forward in the localhost
+ * 
+ * @param {int} _amount Number of blocks to mine in the local blockchain
+ * 
+ * @dev console.log(`Moved forward ${_amount} blocks.`)
+ */
+export const fastForwardBlocks = async (_amount) => {
   const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545')
   for (let i = 0; i < _amount; i++) {
     await provider.send('evm_mine', [])
   }
-  // Console log number of blocks moved forward
-  // console.log(`Moved forward ${_amount} blocks.`);
 }
 
-module.exports = fastForwardBlocks
+
+/**
+ * Move time forward in the localhost
+ * 
+ * @param {int} _seconds Time in seconds to be increased for the local blockchain
+ * 
+ * @dev console.log(`Moved forward ${_seconds} seconds.`)
+ */
+export const speedUpSeconds = async (_seconds) => {
+  const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545')
+  await provider.send('evm_increaseTime', [_seconds])
+}
