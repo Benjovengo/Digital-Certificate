a70977551e376bc946e15d905f59647a27cc824b
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-18 07:28:11 -0300

    add expertise section

--- front-end/src/components/ui/ExpertiseLevel/ExpertiseLevel.jsx
+++ front-end/src/components/ui/ExpertiseLevel/ExpertiseLevel.jsx
@@ -1,8 +1,140 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Container, Row, Col } from "reactstrap";
 
+import './ExpertiseLevel.css' // CSS Style
+
+import { addProposal } from '../../../scripts/governance/propose';
+import { fetchDaoParams } from '../../../scripts/governance/dao-parameters';
+
+
+/**
+ * React component
+ * 
+ * @returns React component
+ *  @ dev The elements of the component are based
+ *        on the ExpertiseClusters smart contract
+ */
 const ExpertiseLevel = () => {
+  /**
+   * Definitions
+   */
+
+
+  /**
+   * Submit proposal
+   * 
+   * @param {event} e Submitting form event
+   * @dev The event has all the information about the
+   *      submission of all the fields inside the form
+   */
+  const handleSubmitProposal = async (e) => {
+    e.preventDefault()
+    
+    const functionToCall = e.target.functionToCall.value
+    const weight1 = Number(e.target.weight1.value)
+    const weight2 = Number(e.target.weight2.value)
+    const weight3 = Number(e.target.weight3.value)
+    const args = [1, weight2]
+    const description = e.target.description.value
+
+    await addProposal(functionToCall, args, description)
+  }
+
+
+  /**
+   * Submit votes
+   * 
+   * @param {event} e Submitting form event
+   * @dev The event has all the information about the
+   *      submission of all the fields inside the form
+   */
+  const handleSubmitVote = async (e) => {
+    e.preventDefault()
+    
+    console.log('Voting placeholder.')
+  }
+
+
+   /**
+   * Submit queue and execute
+   * 
+   * @param {event} e Submitting form event
+   * @dev The event has all the information about the
+   *      submission of all the fields inside the form
+   */
+   const handleSubmitExecution = async (e) => {
+    e.preventDefault()
+    
+    console.log('Queueing and execution placeholder.')
+  }
+
+
+
   return (
-    <div>ExpertiseLevel</div>
+    <>
+      <section className='dao__wrapper'>
+        <Container fluid>
+          <Row>
+            <Col>
+              <h1>Expertise</h1>
+              <h2>Levels of Expertise</h2>
+              <h2>Your Expertise</h2>
+              {/* <div>Expertise Threshold: {weights[0]}, {weights[1]}, {weights[2]} </div> */}
+            </Col>
+          </Row>
+          {/** Add proposal */}
+          <Row>
+            <Col>
+              <h2>Add proposal</h2>
+              <form onSubmit={handleSubmitProposal}>
+                <label htmlFor="functionToCall">Function </label>
+                <select id="functionToCall" name="functionToCall">
+                  <option value="storeExpertiseThreshold">Change Expertise Threshold</option>
+                </select>
+                <label htmlFor="weight1">Weight 1 </label>
+                <input type="number" id="weight1" name="weight1" defaultValue={1}/>
+                <label htmlFor="weight2">Weight 2 </label>
+                <input type="number" id="weight2" name="weight2" defaultValue={1}/>
+                <label htmlFor="weight3">Weight 3 </label>
+                <input type="number" id="weight3" name="weight3" defaultValue={1}/>
+                <label htmlFor="description">Description </label>
+                <input type="text" id="description" name="description" placeholder='Describe action to be proposed.'/>
+                <button type='submit'>Add proposal</button>
+              </form>
+            </Col>
+          </Row>
+          {/** Vote in a proposal */}
+          <Row>
+            <Col>
+              <h2>Vote</h2>
+              <form onSubmit={handleSubmitVote}>
+                <label htmlFor="vote">Vote:</label>
+                <select name="proposalSelect" id="proposalSelect"></select>
+                <select id="vote" name="vote">
+                  <option value="1">Yes</option>
+                  <option value="0">No</option>
+                  <option value="2">Abstain</option>
+                </select>
+                <label htmlFor="voteReason">Reason </label>
+                <input type="text" id="voteReason" name="voteReason"/>
+                <button type='submit'>Vote</button>
+              </form>
+            </Col>
+          </Row>
+          {/** Queue and Execute */}
+          <Row>
+            <Col>
+              <h2>Queue and Execute</h2>
+              <form onSubmit={handleSubmitExecution}>
+                <label htmlFor="selectExecuteProposal">Execute:</label>
+                <select name="selectExecuteProposal" id="selectExecuteProposal"></select>
+                <button type='submit'>Execute</button>
+              </form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
   )
 }
 
