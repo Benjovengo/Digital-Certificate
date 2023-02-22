a084b1d87acc714f2cf74330eee69ea412edb4b4
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-22 09:46:14 -0300

    add test to propose function

--- blockchain/scripts/testings/governance/05-Functionality.js
+++ blockchain/scripts/testings/governance/05-Functionality.js
@@ -1,31 +1,87 @@
 const { expect } = require('chai')
+const { ethers } = require('hardhat')
+const { mine } = require("@nomicfoundation/hardhat-network-helpers");
 
-const proposeAction = require('../../propose')
-const voteAction = require('../../vote')
 
+const expertiseClustersTesting = async () => {
 
-let proposal
-let votingResult
+  let votingToken
+  let timeLock
+  let governorContract
+  let expertiseClusters
 
-const governanceFunctionality = async () => {
-  describe('Functionality Test', () => {
+  let proposalId
 
-    it('Add a proposal (proposal state = 0: Pending).', async () => {
-      proposal = await proposeAction()
-      const proposalState = proposal[1]
+  describe('Expertise Clusters Contract', () => {
+    // Run before each test
+    beforeEach(async () => {
+      // Setup accounts - to get signers use `const signers = await ethers.getSigners()`
+      const [deployer, account01] = await ethers.getSigners()
+      // Deploy VotingToken
+      const VotingToken = await ethers.getContractFactory('VotingToken')
+      votingToken = await VotingToken.deploy()
+
+      /// Definitions for the TimeLock contract
+      const MIN_DELAY = 3600
+      const PROPOSERS = []
+      const EXECUTORS = []
+      // Deploy TimeLock
+      const TimeLock = await ethers.getContractFactory('TimeLock')
+      timeLock = await TimeLock.deploy(MIN_DELAY, PROPOSERS, EXECUTORS)
+
+      /**
+       * Governor Contract Deployment
+       */
+      /// Definitions for the Governor Contract
+      const VOTING_DELAY = 1 // blocks
+      const VOTING_PERIOD = 15 // blocks
+      const QUORUM_PERCENTAGE = 4 // percentage
+      /// Deploy Governor contract
+      const GovernorContract = await ethers.getContractFactory('GovernorContract')
+      governorContract = await GovernorContract.deploy(global.votingToken.address, global.timeLock.address, VOTING_DELAY, VOTING_PERIOD, QUORUM_PERCENTAGE)
+
+      // Deploy ExpertiseClusters
+      const ExpertiseClusters = await ethers.getContractFactory('ExpertiseClusters')
+      expertiseClusters = await ExpertiseClusters.deploy()
+    })
+
+    it('Propose.', async () => {
+      /// @dev Arguments
+      const _functionToCall = 'storeCertificateWeight'
+      const _args = [10, 12, 14, 16] // has to use the same in the queue-and-execute
+      const _proposalDescription = 'Debug description'
+
+      /// @notice Encode the function to be called
+      /// @dev <target_contract>.interface.encodeFunctionData(<function_name_string>,[<arguments>])
+      const encodedFunctionCall = expertiseClusters.interface.encodeFunctionData(_functionToCall, [_args])
+      const EXPERTISE_CONTRACT_ADDRESS = await expertiseClusters.address
+
+      /// @dev Display the information about the function to be called in the target contract
+      /* console.log(`   \x1b[34m*\x1b[37m Proposal Description: ${_proposalDescription}`)
+      console.log(`   \x1b[34m*\x1b[37m Function to call: ${_functionToCall}`)
+      console.log(`   \x1b[34m*\x1b[37m Args: ${_args}`)
+      console.log(`   \x1b[34m*\x1b[37m Encoded Function Call: ${encodedFunctionCall}`) */
+
+      /// @notice Add the proposal
+      const proposeTx = await governorContract.propose(
+        [EXPERTISE_CONTRACT_ADDRESS],
+        [0],
+        [encodedFunctionCall],
+        _proposalDescription
+      );
+      /// @notice Get the response of the proposal
+      const proposeReceipt = await proposeTx.wait();
+
+      /// @notice Get the ID of the proposal
+      proposalId = proposeReceipt.events[0].args.proposalId;
+
+      await mine(2)
+
+      const proposalState = await governorContract.state(proposalId);
       expect(proposalState).to.equal(1)
     })
-
-    it('Vote (proposal state = : ).', async () => {
-      /* votingResult = await voteAction()
-      console.log('Proposal state: ', votingResult) */
-    })
-
-    it('Queue and Execute.', async () => {
-    })
-
+    
   })
-
 }
 
-module.exports = governanceFunctionality
+module.exports = expertiseClustersTesting
